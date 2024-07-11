73888bab4776fde2ffc883e865057eb258b40fd5
Priyank Katiyar <priyankkatiyar11@gmail.com>
2024-07-12 01:40:32 +0530

    .

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://taskmanager-backend-c2ep.onrender.com/graphql',
   cache: new InMemoryCache(),
 });
 
